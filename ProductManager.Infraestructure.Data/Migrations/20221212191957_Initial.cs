5d6eabed658e7a894c8a649750e41d1b36bf56dd
pedro-h-soares <75043682+pedro7744@users.noreply.github.com>
2022-12-12 16:23:31 -0300

    Initial Migration

---
+++ ProductManager.Infraestructure.Data/Migrations/20221212191957_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductManager.Infraestructure.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    ManufacturingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpiringDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProviderId = table.Column<int>(type: "int", nullable: false),
+                    ProviderDescription = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ProviderCnpj = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
