39106c2f2bd64b3fb140c902c02d301d9de480f5
pedro-h-soares <75043682+pedro7744@users.noreply.github.com>
2022-12-12 16:23:01 -0300

    Changing database to SQLServer

--- ProductManager.Presentation/Startup.cs
+++ ProductManager.Presentation/Startup.cs
@@ -1,13 +1,19 @@
-using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using ProductManager.Application.Interfaces;
+using ProductManager.Application.Service;
+using ProductManager.Domain.Core.Interfaces.Repositories;
+using ProductManager.Domain.Core.Services;
+using ProductManager.Domain.Services.Services;
 using ProductManager.Infraestructure.Data;
+using ProductManager.Infraestructure.Repository.Repositories;
+using ProductManager.Infrastruture.CrossCutting.Adapter.Interfaces;
+using ProductManager.Infrastruture.CrossCutting.Adapter.Map;
 
 namespace ProductManager.Presentation
 {
@@ -25,7 +31,7 @@ namespace ProductManager.Presentation
         {
 
             string connectionString = Configuration.GetConnectionString("DefaultConnection");
-            services.AddDbContext<SqlContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+            services.AddDbContext<SqlContext>(options => options.UseSqlServer(connectionString));
             services.AddMemoryCache();
 
 
@@ -34,6 +40,11 @@ namespace ProductManager.Presentation
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductManager.Presentation", Version = "v1" });
             });
+
+            services.AddScoped<IApplicationServiceProduct, ApplicationServiceProduct>();
+            services.AddScoped<IServiceProduct, ServiceProduct>();
+            services.AddScoped<IRepositoryProduct, RepositoryProduct>();
+            services.AddScoped<IMapperProduct, MapperProduct>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,11 +67,6 @@ namespace ProductManager.Presentation
             {
                 endpoints.MapControllers();
             });
-        }
-
-        public void ConfigureContainer(ContainerBuilder builder)
-        {
-            builder.RegisterModule(new ModuleIOC());
         }
     }
 }
